ab37013048c6e6a389ec177fe15121fd857b99b9
박민한 <alsgks1218@naver.com>
2022-11-24 10:49:38 +0900

    initial setting

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,3 @@
-# React-router-dom 6 example
-
-## 기존 형태와 비슷하게 하려면
-
-```jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./screens/About";
@@ -21,6 +16,3 @@ const Router = () => {
 };
 
 export default Router;
-```
-
-BrowserRouter로 감싼 다음 Routes에 Route들을 정의해주면 된다.
